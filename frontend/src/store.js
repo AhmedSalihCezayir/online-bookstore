7bd0d9ccd856ce3b94736f37af917cec479fbcc9
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2023-05-17 13:06:17 +0300

    review deleted

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,6 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
-  productReviewCreateReducer,
   productTopRatedReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
@@ -35,7 +34,6 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
-  productReviewCreate: productReviewCreateReducer,
   productTopRated: productTopRatedReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
